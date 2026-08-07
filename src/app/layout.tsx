f8fd737d9f17c7ee0e51c13c30134b517ce7ea41
Adrián García Arranz <adrigar1111@gmail.com>
2026-08-07 12:08:21 +0200

    configurar base de shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="es" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html
+      lang="es"
+      className={`dark ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
